f8fcc1edcf848b526f5adffa9109a0fb74f406a6
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2023-07-19 16:00:21 +1200

    Update Perceptron.js

--- classes/Perceptron.js
+++ classes/Perceptron.js
@@ -1,17 +1,17 @@
 class Perceptron {
     constructor() {
-        this.weights = Math.random(); // random weights
-        this.bias = Math.random(); // random bias
+        this.weights = Math.random();
+        this.bias = Math.random();
         this.learningRate = 0.1; 
     }
 
     predict(input) {
         let weightedSum = this.weights * input + this.bias;
+        console.log(weightedSum);
         return this.activationFunction(weightedSum); 
     }
 
     activationFunction(x) {
-        // activation function
         return x >= 0 ? 1 : 0;
     }
 
@@ -19,11 +19,10 @@ class Perceptron {
         let prediction = this.predict(input);
         let error = target - prediction;
 
-        // Update weights and bias
+        // Update weights and bias (back propagation)
         this.weights += this.learningRate * error * input;
         this.bias += this.learningRate * error;
     }
 }
-
 
 module.exports = Perceptron;
