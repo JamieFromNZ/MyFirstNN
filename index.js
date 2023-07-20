f384eecd8d0836a4960a21b605b0e27f2e829390
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2023-07-21 10:19:57 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const SimpleWordPredictor = require('./classes/SimpleWordPredictor');
 const wtf = require('wtf_wikipedia');
 const readline = require('readline');
+const SimpleWordPredictor = require('./classes/SimpleWordPredictor');
 
-// So that we can use it as a global variable, I don't enjoy throwing it around as a parameter
-let model = undefined
+// Global variable
+let model = undefined;
 
 // Helper function to preprocess text for training
 function preprocessText(text) {
@@ -16,9 +16,7 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-// Fetch data from a Wikipedia page
-wtf.fetch('Artificial_intelligence').then((doc) => {
-    // Extract the plain text from the Wikipedia page
+wtf.fetch('World_War_II').then((doc) => {
     const text = doc.text();
     // Preprocess the text to get the training data
     const trainingData = preprocessText(text);
@@ -32,7 +30,7 @@ wtf.fetch('Artificial_intelligence').then((doc) => {
         }
     }
 
-    model = new SimpleWordPredictor(index, wordToNumber);
+    model = new SimpleWordPredictor(index, wordToNumber, [50, 30]);
 
     // Train the model
     for (let i = 0; i < 10000; i++) {
@@ -43,40 +41,37 @@ wtf.fetch('Artificial_intelligence').then((doc) => {
         }
     }
 
-    // Predict the next word after each word in the text
-    for (let i = 0; i < trainingData.length - 1; i++) {
-        let currentWord = wordToNumber[trainingData[i]];
-        let predictedNextWord = model.predict(currentWord);
-        let predictedWord = Object.keys(wordToNumber).find((word) => wordToNumber[word] === Math.round(predictedNextWord));
-        console.log(`Current word: ${trainingData[i]}, predicted next word: ${predictedWord}`);
+    // Function to generate text from multiple seed words
+    function generateText(model, seedWords, length = 10) {
+        const generatedText = seedWords.slice(); // Copy seed words to the generated text array
+        let currentWordIndex = seedWords.map(word => wordToNumber[word]);
+
+        // Add words to the array
+        for (let i = 0; i < length; i++) {
+            const predictedNextWords = currentWordIndex.map(index => model.predict(index));
+            const predictedWordIndices = predictedNextWords.map(nextWord =>
+                Object.values(model.wordToNumber).findIndex(value => value === Math.round(nextWord))
+            );
+            const predictedWords = predictedWordIndices.map(index => Object.keys(model.wordToNumber)[index]);
+
+            generatedText.push(...predictedWords);
+            currentWordIndex = predictedWordIndices;
+        }
+
+        return generatedText.join(' ');
+    }
+
+    function askQuestion() {
+        rl.question('Enter the seed words (space-separated) for text generation: ', (seedWordsInput) => {
+            const seedWords = seedWordsInput.split(/\s+/);
+            const generatedText = generateText(model, seedWords, 20);
+            console.log(`Generated text: ${generatedText}`);
+
+            askQuestion();
+        });
     }
 
     askQuestion();
 }).catch((err) => {
     console.error(err);
 });
-
-function generateText(model, seedWord, length = 10) {
-    const generatedText = [seedWord];
-    let currentWord = model.wordToNumber[seedWord];
-
-    // Add words to array, 2% chargeee aargh
-    for (let i = 0; i < length; i++) {
-        const predictedNextWord = model.predict(currentWord);
-        const predictedWord = Object.keys(model.wordToNumber).find((word) => model.wordToNumber[word] === Math.round(predictedNextWord));
-
-        generatedText.push(predictedWord);
-        currentWord = model.wordToNumber[predictedWord];
-    }
-
-    return generatedText.join(' ');
-}
-
-function askQuestion() {
-    rl.question('Enter the seed word for text generation: ', (seedWord) => {
-        const generatedText = generateText(model, seedWord, 20);
-        console.log(`Generated text: ${generatedText}`);
-
-        askQuestion();
-    });
-}
